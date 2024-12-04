e8cb7afe20a723b9a09138ce5607b85e053215ef
Rachel Breeze <hello@rachelbreeze.dev>
2024-12-04 10:22:54 +0000

    Added Total By Column

--- src/Day 4/Parser.cs
+++ src/Day 4/Parser.cs
@@ -12,13 +12,61 @@ internal sealed class Parser : IParser
             throw new ArgumentNullException(nameof(wordSearch));
         }
 
+        var grid = ConvertoGrid(wordSearch);
 
-        var total = CaluclateTotalXmasByLine(wordSearch);
-
+        var total = CalculateTotalXmasByLine(wordSearch);
+        total += CalculateTotaleXmasByColumn(grid);
         return total;
 
     }
-    internal protected int CaluclateTotalXmasByLine(StringCollection wordSearch)
+
+    internal protected int CalculateTotaleXmasByColumn(Dictionary<int, char[]> grid)
+    {
+        var rows = ConvertColumnsToRows(grid);
+        return CalculateTotalXmasByLine(rows);
+    }
+
+    internal protected StringCollection ConvertColumnsToRows(Dictionary<int, char[]> grid)
+    {
+        if (grid == null)
+        {
+            throw new ArgumentNullException(nameof(grid));
+        }
+        if (grid.Count == 0)
+        {
+            return new StringCollection();
+        }
+        var totalColumns = grid[0].Length;
+        var rows = new StringCollection();
+
+        for (int i = 0; i < totalColumns; i++)
+        {
+            var row = string.Empty;
+            foreach (var column in grid)
+            {
+                row += column.Value[i];
+            }
+            rows.Add(row);
+        }
+
+        return rows;
+    }
+
+    internal protected Dictionary<int, char[]> ConvertoGrid(StringCollection wordSearch)
+    {
+        if (wordSearch == null)
+        {
+            throw new ArgumentNullException(nameof(wordSearch));
+        }
+
+        var grid = new Dictionary<int, char[]>();
+        for (int i = 0; i < wordSearch.Count; i++)
+        {
+            grid.Add(i, wordSearch[i].ToCharArray());
+        }
+        return grid;
+    }
+    internal protected int CalculateTotalXmasByLine(StringCollection wordSearch)
     {
         int total = 0;
         foreach (var line in wordSearch)
--- tests/Day 4/ParserTests.cs
+++ tests/Day 4/ParserTests.cs
@@ -35,4 +35,128 @@ internal class ParserTests
         var result = parser.Reverse("xmas");
         Assert.AreEqual("samx", result);
     }
+
+    [Test]
+    public void ConvertoGrid_ValidInput_ReturnsCorrectGrid()
+    {
+        // Arrange
+        var parser = new Parser();
+        var wordSearch = new StringCollection { "xmas", "test", "grid" };
+
+        // Act
+        var result = parser.ConvertoGrid(wordSearch);
+
+        // Assert
+        Assert.AreEqual(3, result.Count);
+        Assert.AreEqual(new char[] { 'x', 'm', 'a', 's' }, result[0]);
+        Assert.AreEqual(new char[] { 't', 'e', 's', 't' }, result[1]);
+        Assert.AreEqual(new char[] { 'g', 'r', 'i', 'd' }, result[2]);
+    }
+
+    [Test]
+    public void ConvertoGrid_EmptyInput_ReturnsEmptyGrid()
+    {
+        // Arrange
+        var parser = new Parser();
+        var wordSearch = new StringCollection();
+
+        // Act
+        var result = parser.ConvertoGrid(wordSearch);
+
+        // Assert
+        Assert.AreEqual(0, result.Count);
+    }
+
+    [Test]
+    public void ConvertoGrid_NullInput_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var parser = new Parser();
+        StringCollection wordSearch = null;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => parser.ConvertoGrid(wordSearch));
+    }
+
+    [Test]
+    public void ConvertColumnsToRows_ValidGrid_ReturnsCorrectRows()
+    {
+        // Arrange
+        var parser = new Parser();
+        var grid = new Dictionary<int, char[]>
+            {
+                { 0, new char[] { 'a', 'b', 'c' } },
+                { 1, new char[] { 'd', 'e', 'f' } },
+                { 2, new char[] { 'g', 'h', 'i' } }
+            };
+
+        var expected = new StringCollection { "adg", "beh", "cfi" };
+
+        // Act
+        var result = parser.ConvertColumnsToRows(grid);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void ConvertColumnsToRows_EmptyGrid_ReturnsEmptyStringCollection()
+    {
+        // Arrange
+        var parser = new Parser();
+        var grid = new Dictionary<int, char[]>();
+
+        var expected = new StringCollection();
+
+        // Act
+        var result = parser.ConvertColumnsToRows(grid);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void ConvertColumnsToRows_NullGrid_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var parser = new Parser();
+        Dictionary<int, char[]> grid = null;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => parser.ConvertColumnsToRows(grid));
+    }
+
+    [Test]
+    public void CalculateTotaleXmasByColumn_ValidGrid_ReturnsCorrectTotal()
+    {
+        var parser = new Parser();
+        var grid = new Dictionary<int, char[]>
+            {
+                { 0, new char[] { 'x', 'm', 'a', 's', 'x', 's' } },
+                { 1, new char[] { 's', 'a', 'm', 'x', 'm', 'a' } },
+                { 2, new char[] { 'a', 'x', 's', 'm', 'a', 'm' } },
+                { 3, new char[] { 'm', 's', 'x', 'a', 's', 'x' } }
+            };
+
+        var result = parser.CalculateTotaleXmasByColumn(grid);
+
+        Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void CalculateTotaleXmasByColumn_EmptyGrid_ReturnsZero()
+    {
+        var grid = new Dictionary<int, char[]>();
+        var parser = new Parser();
+        var result = parser.CalculateTotaleXmasByColumn(grid);
+
+        Assert.AreEqual(0, result);
+    }
+
+    [Test]
+    public void CalculateTotaleXmasByColumn_NullGrid_ThrowsArgumentNullException()
+    {
+        var parser = new Parser();
+        Assert.Throws<ArgumentNullException>(() => parser.CalculateTotaleXmasByColumn(null));
+    }
 }
